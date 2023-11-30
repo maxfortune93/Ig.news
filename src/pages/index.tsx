33ebad9f924bc5ff312377ed57f8b799138417eb
MAROUANE PONDIKPA <79520932+maxfortune93@users.noreply.github.com>
2023-11-30 16:54:01 -0300

    Initialize IgNews Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+    <Head>
+      <title>Home | Ig.news</title>
+    </Head>
+    <h1>Hello Marou</h1>
+    </>
+  )
+}
