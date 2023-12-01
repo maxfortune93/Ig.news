2a3cc0256793d0753d2fd24ff62b4bd1e1e76bcf
MAROUANE PONDIKPA <79520932+maxfortune93@users.noreply.github.com>
2023-12-01 17:04:34 -0300

    implement home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from "next/head";
+
+import styles from "./home.module.scss";
+import { SubscribeButton } from "../componentes/SubscribeButton";
 export default function Home() {
   return (
     <>
     <Head>
       <title>Home | Ig.news</title>
     </Head>
-    <h1>Hello Marou</h1>
+    
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏 Hey, welcome</span>
+        <h1>News about the <span>React</span> world </h1>
+        <p>
+          Get access to all the publications <br />
+          <span>for $9.90 month</span>
+        </p>
+        <SubscribeButton />
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding" />
+    </main>
     </>
   )
 }
